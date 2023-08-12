06cd299fa03a658b25000962bda354bb00514fa0
zeroxerCpp <90362622+zeroxerCpp@users.noreply.github.com>
2023-08-12 14:43:36 +0800

    Update SecretInfo.java

--- ShellStudy/src/com.zks.learn/constant/SecretInfo.java
+++ ShellStudy/src/com.zks.learn/constant/SecretInfo.java
@@ -2,5 +2,5 @@ package constant;
 
 public class SecretInfo {
     //钱包私钥
-    public static final String privateKey = "dc97fedb546c918a5811fadb0f67f101771ed91e0c9374dac8065aa6f49ec403";
+    public static final String privateKey = "";
 }
